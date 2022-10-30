dbfab62e52f718ebdf53a98e69eff6f36c4a2d11
David Lyons <davidbrucelyons@gmail.com>
2022-10-30 09:59:46 -0400

    Update processing_TEs_step3_make_tables.R

--- processing_TEs_step3_make_tables.R
+++ processing_TEs_step3_make_tables.R
@@ -16,21 +16,6 @@ cov.for.AB<-dcast(melt.tog.long, teId ~ genotype + generation + rep,
                       value.var='cplust', fun.aggregate=sum)
 
 
-# get TE length
-el<-read.table(file='/kingsley_1/3-analysis/bsseq/intermediate_file/met1/length.txt')
-names(el)[10]='teLength'
-names(el)[11]='ID'
-el<-el[,c(10,11)]
 
 
 
-write.table(cov.for.AB.with.len, "/kingsley_1/3-analysis/bsseq/intermediate_file/for_AB/coverage_with_length.forAB.txt", sep="\t", 
-            row.names=FALSE, col.names=TRUE, quote=FALSE)
-
-# remove all but >2kbp for AB's ease?  i sent it to her, not sure if needed
-mcg.for.AB.w.met1.long<-mcg.for.AB.w.met1.long[mcg.for.AB.w.met1.long$teLength>2000,]
-
-write.table(mcg.for.AB.w.met1.long[,c(1:64)], 
-            "/kingsley_1/3-analysis/bsseq/intermediate_file/for_AB/
-            mcg_met1_added.forAB.gt2000bp.txt", sep="\t", row.names=FALSE, 
-            col.names=TRUE, quote=FALSE)
